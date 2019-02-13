8148379b301e8e9cbbe745cfcbb8bca458caa144
visualstudio <visualstudio@122-PC03>
2019-02-13 09:15:26 -0500

    Commit12

--- test1/Program.cs
+++ test1/Program.cs
@@ -10,6 +10,35 @@ namespace test1
     {
         static void Main(string[] args)
         {
+            new DogHouse().Run();
+
+        }
+    }
+}
+class Dog
+{   //  Dog class is ADT
+    public string DogName;
+    public int age;
+    public string favoriteToy;
+    public int milesWalkedPerDay;
+}
+class DogHouse
+{
+    Dog D1, D2, D3, D4, Temp;
+    string[] DogNames;
+    Dog[] Dogs;
+
+    public void Run()
+    {
+
+
+        Dog[] Dogs = new Dog[5];
+        DogNames = new string[5] { "Peanut", "Fifi", "Fido", "Clearence", "Bigi" };
+
+        for (int i = 0; i < Dogs.Length; i++)
+        {
+            Dogs[i] = new Dog();
+            Dogs[i].DogName = DogNames[i];
         }
     }
 }
